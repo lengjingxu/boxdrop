78545cb43b9289f7463a9ec2c1f9d58e651147d9
lengjingxu <lengjingxu@gmail.com>
2015-09-24 00:41:13 +0200

    进度条
    todo json 显示进度条

--- download.php
+++ download.php
@@ -3,15 +3,20 @@ $fileurl=  $_POST['dropurl'];
 
 $filename= $_POST['filename'];
 
+
+function progress ($resource, $download_size, $downloaded_size) {
+    $return = $downloaded_size/$download_size;
+	$return["json"] = json_encode($return);
+	echo json_encode($return);
+};
     $fp_output = fopen('./'."$filename", 'w');
     $ch = curl_init($fileurl);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     curl_setopt($ch, CURLOPT_FILE, $fp_output);
+	curl_setopt($ch,CURLOPT_NOPROGRESS,0);
+	curl_setopt($ch,CURLOPT_PROGRESSFUNCTION,'progress');
     curl_exec($ch);
     curl_close($ch);  
 	
- echo "$filename";
-echo "<br>";
-echo "$fileurl";
 ?>
--- test.php
+++ test.php
@@ -1,22 +1,26 @@
 <?php
-$fileurl=  "https://dl.dropboxusercontent.com/1/view/pnadlale8qo0122/MBA E buiness/Droit/DroitE-Business.docx"; 
+$fileurl=  "http://blog.chinaunix.net/uid-26696966-id-3181311.html"; 
 
 $filename= substr($fileurl,strripos($fileurl,"/")+1);
 
 
+ echo "$filename";
+echo "<br>";
+echo "$fileurl";
 
-
-  
-    $fp_output = fopen('./'.$filename, 'w');
+function downloadProgress ($resource, $download_size, $downloaded_size) {
+    echo 'download_size: ' . $download_size . '; downloaded_size: ' . $downloaded_size . ';<br>';
+};
+    $fp_output = fopen('./'."$filename", 'w');
     $ch = curl_init($fileurl);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	
     curl_setopt($ch, CURLOPT_FILE, $fp_output);
+	curl_setopt($ch,CURLOPT_NOPROGRESS,0);
+	curl_setopt($ch,CURLOPT_PROGRESSFUNCTION,'downloadProgress');
     curl_exec($ch);
     curl_close($ch);  
+	fclose($fp_output);
 	
-	
-	
-	
+echo "ok";
  
 ?>
